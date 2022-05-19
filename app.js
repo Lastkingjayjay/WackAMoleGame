58d09e59d8bc8685f33a755be5eb8631cd92f3c2
Junior R Joseph <93812419+Lastkingjayjay@users.noreply.github.com>
2022-05-18 23:50:56 -0400

    finished Game
    completed game and added custom styling

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const timeLeft = document.querySelector("#time-left");
 const score = document.querySelector("#score");
 
 let result = 0;
+let hitPositiion;
+let currentTime = 60;
+let timerId = null;
 
 function randomSquare() {
   squares.forEach((square) => {
@@ -12,10 +15,32 @@ function randomSquare() {
 
   let randomSquare = squares[Math.floor(Math.random() * 9)];
   randomSquare.classList.add("mole");
+  hitPositiion = randomSquare.id;
 }
 
+squares.forEach((square) => {
+  square.addEventListener("mousedown", () => {
+    if (square.id == hitPositiion) {
+      result++;
+      score.textContent = result;
+      hitPositiion = null;
+    }
+  });
+});
+
 function moveMole() {
-  let timerID = null;
-  timerID = setInterval(randomSquare, 500);
+  timerId = setInterval(randomSquare, 500);
 }
 moveMole();
+
+function countDown() {
+  currentTime--;
+  timeLeft.textContent = currentTime;
+
+  if (currentTime == 0) {
+    clearInterval(timerId);
+    clearInterval(countDownTimerId);
+    alert("GAME OVER! Your final score is " + result);
+  }
+}
+let countDownTimerId = setInterval(countDown, 1000);
